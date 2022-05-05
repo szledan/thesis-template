5bef787e5fc100498946f18a930a7d04a7360097
Szilard Ledan <szledan@inf.u-szeged.hu>
2022-05-06 01:27:31 +0300

    Improve install-deps.sh

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-# For generate the thesis.
-sudo apt-get install texlive-full
+_apps="cmake texlive-full"
 
-if [[ ${1} == "--dev" ]] ; then
-  echo "Install packages for developing..."
-  sudo apt-get install okular geany-plugin-spellcheck hunspell-hu
+for _arg in "$@"; do
+  case ${_arg} in
+    "--build")
+      _apps="${_apps} inkscape";;
+    "--dev")
+      _apps="${_apps} okular geany-plugin-spellcheck hunspell-hu";;
+    *)
+        echo "Usage: ${0} [OPTION]"
+        echo ""
+        echo "Option:"
+        echo "  --help   show this help"
+        echo "  --build  install dependencies for re-build whole project"
+        echo "  --dev    install some usefull editor, spellchecker"
+        exit 0;;
+  esac
+done
+
+sudo apt-get install $_apps
+
+if [[ ${?} != 0 ]] ; then
+  echo "Something went wrong!!!"
+else
+  echo "Done!"
 fi
-
-echo "Done!"
